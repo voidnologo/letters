376371e6e8dc1e084c9e63395224b8b2b37de105
voidnologo <void.no.logo@icloud.com>
2015-09-10 22:18:03 -0500

    Remove 'black' as font color

--- color.py
+++ color.py
@@ -1,7 +1,6 @@
 class Colorize:
     """Color ANSI codes"""
     colors = {
-        'FG_BLACK': '\033[30m',
         'FG_RED': '\033[31m',
         'FG_GREEN': '\033[32m',
         'FG_YELLOW': '\033[33m',
@@ -9,7 +8,6 @@ class Colorize:
         'FG_MAGENTA': '\033[35m',
         'FG_CYAN': '\033[36m',
         'FG_WHITE': '\033[37m',
-        'FG_BOLD_BLACK': '\033[90m',
         'FG_BOLD_RED': '\033[91m',
         'FG_BOLD_GREEN': '\033[92m',
         'FG_BOLD_YELLOW': '\033[93m',
